889b14edba67d0cf395408314eae8e3bc3c9d6fe
Антон Ефимов <Di_oXid@mail.ru>
2022-09-28 21:21:34 +0500

    Add HW Seminar1 Task4

--- HomeWork/Seminar1/Task2.py
+++ HomeWork/Seminar1/Task2.py
@@ -3,5 +3,5 @@
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            result = (not (x or y or z)) == (not (x) and not y and not z)
+            result = (not (x or y or z)) == (not x and not y and not z)
             print(result)
