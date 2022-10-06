8fbf96718d2caa5681c7c47a0124e859b7a67d27
Антон Ефимов <Di_oXid@mail.ru>
2022-10-06 20:58:58 +0500

    Add HW Seminar4 Task2

--- HomeWork/Seminar4/Task1.py
+++ HomeWork/Seminar4/Task1.py
@@ -1,6 +1,6 @@
 # Задание 1 Вычислить число π c заданной точностью d
 import math
-accuracy_string = input('Введите точность вывода числа π в в виде 0.0...1 - ')
+accuracy_string = input('Введите точность вывода числа π без округления в в виде 0.0...1 - ')
 pi_string = str(math.pi)
 pi_accuracy = float(pi_string[0:len(accuracy_string)])
 print(pi_accuracy)
---
+++ HomeWork/Seminar4/Task2.py
@@ -0,0 +1,21 @@
+# Задание 2 Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
+
+def view_uniq_elem_list (string_number):
+    list_number = string_number.split(' ')
+    print(list_number)
+    list_number_len = len(list_number)
+    counter_i = 0
+    while counter_i < list_number_len:
+        counter_j = counter_i + 1
+        while list_number.count(list_number[counter_i]) > 1:
+            if list_number[counter_i] == list_number[counter_j]:
+                list_number.pop(counter_j)
+                list_number_len -= 1
+            else:
+                counter_j+=1
+        counter_i+=1
+    print(list_number)
+
+string_number = input('Введите последовательность чисел разделяя пробелом - ')
+view_uniq_elem_list(string_number)
