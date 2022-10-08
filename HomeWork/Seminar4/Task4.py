96b1a975328e76fa967eeedffad5bd32695aa6d9
Антон Ефимов <Di_oXid@mail.ru>
2022-10-08 19:12:43 +0500

    Add HW Seminar4 Task4 fix1

--- HomeWork/Seminar4/Task4.py
+++ HomeWork/Seminar4/Task4.py
@@ -6,21 +6,24 @@ list_ratio = []
 degree = random.randint(2, 5) #к=2...5
 result = ''
 for i in range(degree + 1):
-    list_ratio.append(random.randint(0, 100))
+    list_ratio.append(random.randint(0, 5))
 
 for i in range(degree, 0, -1):
-    if list_ratio[i] == 0: #Для пропуска нулевых коэфициентов x
+    if list_ratio[i] == 0:              #Для пропуска нулевых коэфициентов x
         continue
-    result = result + str(list_ratio[i]) + 'x^' + str(i) + '+'
-if list_ratio[0] !=0:
+    elif list_ratio[i] == 1: 
+        result = result + 'x^' + str(i) + '+'
+    else:
+        result = result + str(list_ratio[i]) + 'x^' + str(i) + '+'
+if list_ratio[0] !=0:                   #Для пропуска последнего нулевого коэфициента
     result = result + str(list_ratio[0]) + '=0'
 elif list_ratio[0] ==0:
     result = result  + '=0'
-    result = result.replace('+=0', '') #для удаления лишнего плюса при отсутствии последнего коэфициента
+    result = result.replace('+=0', '')  #для удаления лишнего плюса при отсутствии последнего коэфициента
     result = result  + '=0'
-result = result.replace('^1', '') #для удаления первой степени
+result = result.replace('^1', '')       #для удаления первой степени
 # print(degree)
-# print(list_ratio)
+print(list_ratio)
 print(result)
 with open('text1.txt', 'w+') as data:
     data.write(result)
